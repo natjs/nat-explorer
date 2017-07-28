3d0dde9101b3686f27b50a2159f46298e63bc27c
Acathur <chiahungzhang@gmail.com>
2017-07-28 14:02:49 +0800

    Android: 1.0.1 released
    statistics

---
+++ platforms/android/app/src/main/java/com/instapp/natex/ApiKey.java
@@ -0,0 +1,10 @@
+package com.instapp.natex;
+
+/**
+ * Created by Acathur on 28/07/2017.
+ */
+
+public class ApiKey {
+    public static final String LC_APPID = "Your Leancloud AppId";
+    public static final String LC_APPKEY = "Your Leancloud AppKey";
+}
